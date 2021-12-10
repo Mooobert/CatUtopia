2f6c4472733cd394c194582fb1623aefc1a45c0e
unknown <misha@burnayev.com>
2021-12-09 22:33:10 -0500

    level row size fitting for structure count

--- structureInsertion.java
+++ structureInsertion.java
@@ -1,4 +1,35 @@
 public class structureInsertion {
-    private String[][] generatedUtopia;
-    
+    private int[][] generatedUtopia;
+    private int expansionLevel = 4;
+
+    public int[][] insertRows(int level) {
+        switch (level) {
+            case 0:
+                generatedUtopia = new int[5][];
+                break;
+            case 1:
+                generatedUtopia = new int[7][];
+                break;
+            case 2:
+                generatedUtopia = new int[9][];
+                break;
+            case 3:
+                generatedUtopia = new int[13][];
+                break;
+            case 4:
+                generatedUtopia = new int[17][];
+                break;
+            default:
+                generatedUtopia = new int[5][];
+                break;
+        }
+        return generatedUtopia;
+
+    }
+    // 0 has 7 spaces
+    // 1 has
+    // 2 has
+    // 3 has
+    // 4 has
+    //
 }
